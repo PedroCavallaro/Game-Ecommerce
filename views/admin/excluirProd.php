d30a457146a3240de9163dc20d9c2a22c73a5630
FIRMINOenzo <enzofirmino.camp@gmail.com>
2023-06-14 18:46:40 -0300

    admin update

--- views/admin/excluirProd.php
+++ views/admin/excluirProd.php
@@ -1,2 +1,22 @@
 <?php
-echo $_GET["id"];
+
+$id = $_GET["id"] ?? 0;
+
+if ($id) {
+  try {
+    include_once "../../src/bd.php";
+
+    $bd = connect();
+
+    $sqlImages = "DELETE FROM imagem WHERE codigo_prod = " . $id;
+
+    $sqlProd = "DELETE FROM produto WHERE codigo_prod = " . $id;
+
+    $bd->query($sqlImages);
+    $bd->query($sqlProd);
+
+    header("location:admin.php");
+  } catch (Exception $ex) {
+    header("location:admin.php?err=2");
+  }
+}
